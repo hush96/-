ea3b0a73784eb031cdfc0529afe51bbf44e6ddee
f <fang@qq.com>
2019-12-07 11:37:13 +0800

    分页和筛选

--- public/assets/js/posts.js
+++ public/assets/js/posts.js
@@ -3,7 +3,9 @@ $.ajax({
     url:'/posts',
     success: function(response) {
         let html = template('postsTpl',response);
-        $('#postsBox').html(html)
+        $('#postsBox').html(html);
+        let page = template('pageTpl',response);
+        $('#page').html(page)
         
     }
 });
@@ -11,3 +13,49 @@ function formateDate(date) {
     date = new Date(date);
     return date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate()
 }
+
+function changePage (page) {
+    $.ajax({
+        type:'get',
+        url:'/posts',
+        data: {
+            page:page
+        },
+        success: function(response) {
+            let html = template('postsTpl',response);
+            $('#postsBox').html(html);
+            let page = template('pageTpl',response);
+            $('#page').html(page)
+            
+        }
+    });
+        
+};
+$.ajax({
+    type:'get',
+    url:'/categories',
+    success: function (response) {
+        let html = template('categoryTpl',{data:response})
+        $('#categoryBox').html(html)
+    }
+});
+
+
+$('#filterForm').on('submit',function () {
+    
+    let formData = $(this).serialize();
+    $.ajax({
+        type:'get',
+        url:'/posts',
+        data:formData,
+        success: function(response) {
+            let html = template('postsTpl',response);
+            $('#postsBox').html(html);
+            let page = template('pageTpl',response);
+            $('#page').html(page)
+            
+        }
+    });
+    
+    return false
+})
